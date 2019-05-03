b0ca0ad4846364b86347f0704b2d65b2d090d076
Robert Karl <robertkarljr@gmail.com>
2019-05-02 19:50:34 -0700

    Add bp to yabc module object

--- src/yabc/__init__.py
+++ src/yabc/__init__.py
@@ -1,4 +1,8 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 
+import yabc.server.yabc_api
+
 Base = declarative_base(sqlalchemy.schema.ThreadLocalMetaData())
+
+bp = yabc.server.yabc_api.bp
