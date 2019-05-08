7c6d0a465eadf0a04b3d159d47caef4e6638ecec
Robert Karl <robertkarljr@gmail.com>
2019-05-08 15:16:24 -0700

    CSV and tx updates

--- src/yabc/csv_to_json.py
+++ src/yabc/csv_to_json.py
@@ -5,6 +5,7 @@ Convert coinbase or gemini files to json.
 __author__ = "Robert Karl <robertkarljr@gmail.com>"
 
 import csv
+from yabc import transaction
 
 from dateutil import parser
 
@@ -44,50 +45,59 @@ def clean_gemini_row(tx):
 
 
 def gemini_to_dict(fname):
-    assert type(fname) is str
-    fieldnames = []
-    rawcsv = [i for i in csv.reader(open(fname))]
-    fieldnames = rawcsv[0]
-    with open(fname) as f:
-        ts = [i for i in csv.DictReader(f, fieldnames)]
-        ts = ts[1:]
-        ans = []
-        for tx in ts:
-            item = clean_gemini_row(tx)
-            if item is not None:
-                ans.append(item)
-        return [i for i in ans if i["Type"] == "Buy" or i["Type"] == "Sell"]
-    return []
+    f = open(fname, 'r')
+    return from_gemini(f)
 
+def txs_from_gemini(f):
+    dicts = from_gemini(f)
+    return [transaction.Transaction.FromGeminiJSON(i) for i in dicts]
+
+def txs_from_coinbase(f):
+    dicts = from_coinbase(f)
+    return [transaction.Transaction.FromCoinbaseJSON(i) for i in dicts]
+
+def from_gemini(f):
+    fieldnames = []
+    rawcsv = [i for i in csv.reader(f)]
+    fieldnames = rawcsv[0]
+    f.seek(0)
+    ts = [i for i in csv.DictReader(f, fieldnames)]
+    ts = ts[1:]
+    ans = []
+    for tx in ts:
+        item = clean_gemini_row(tx)
+        if item is not None:
+            ans.append(item)
+    return [i for i in ans if i["Type"] == "Buy" or i["Type"] == "Sell"]
 
 def coinbase_to_dict(fname):
-    filename = fname
-    with open(filename) as f:  # TODO don't open and read this file twice
-        rawcsv = [i for i in csv.reader(f)]
-        if len(rawcsv) < 4:
-            raise ValueError("Invalid CSV file {}, not enough rows.".format(fname))
-        fieldnames = rawcsv[4]
-        assert fieldnames[-2].count("Coinbase") > 0
-        fieldnames[-1] = "Bitcoin Hash"
-        fieldnames[-2] = "Coinbase ID"
+    f = open(fname, 'r')
+    return from_coinbase(f)
 
-    with open(filename) as f:
-        transactions = [i for i in csv.DictReader(f, fieldnames)]
-        # Previously, coinbase column 4 had timestamp.  Documents generated as
-        # of April 2019 have it as the first column.
-        # First meaningful row is 4; 3 has headers so we can check that.
-        assert transactions[3]["Timestamp"] == "Timestamp"
-        transactions = transactions[4:]
-        for i in transactions:
-            assert "Coinbase ID" in i
-            i["Site"] = "Coinbase"
-        USD_and_back = [
-            i
-            for i in transactions
-            if (i["Transfer Total"] is not "" and i["Transfer Total"] is not None)
-        ]
-        return USD_and_back
-    return []
+def from_coinbase(f):
+    rawcsv = [i for i in csv.reader(f)]
+    if len(rawcsv) < 4:
+        raise ValueError("Invalid CSV file {}, not enough rows.".format(fname))
+    fieldnames = rawcsv[4]
+    assert fieldnames[-2].count("Coinbase") > 0
+    fieldnames[-1] = "Bitcoin Hash"
+    fieldnames[-2] = "Coinbase ID"
+    f.seek(0)
+    transactions = [i for i in csv.DictReader(f, fieldnames)]
+    # Previously, coinbase column 4 had timestamp.  Documents generated as
+    # of April 2019 have it as the first column.
+    # First meaningful row is 4; 3 has headers so we can check that.
+    assert transactions[3]["Timestamp"] == "Timestamp"
+    transactions = transactions[4:]
+    for i in transactions:
+        assert "Coinbase ID" in i
+        i["Site"] = "Coinbase"
+    USD_and_back = [
+        i
+        for i in transactions
+        if (i["Transfer Total"] is not "" and i["Transfer Total"] is not None)
+    ]
+    return USD_and_back
 
 
 def get_transactions_to_USD(fname, exchange):
