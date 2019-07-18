97ed3f32d6de8ac5c65ec729240605440563cf35
Robert Karl <robertkarljr@gmail.com>
2019-07-18 12:04:38 -0700

    a

--- setup.py
+++ setup.py
@@ -16,13 +16,14 @@ setuptools.setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3 :: Only",
     ],
 )
