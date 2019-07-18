e2ebe83178c6703c86363319bde21eceaf739bec
Robert Karl <robertkarl@users.noreply.github.com>
2019-07-18 12:05:34 -0700

    Version bump; pypi + shield fix (#52)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(
-    version="0.1.2",
+    version="0.1.3",
     name="yabc",
     python_requires=">=3.5,<3.8",
     author="Seattle Blockchain Solutions",
@@ -16,11 +16,14 @@ setuptools.setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
     ],
 )
