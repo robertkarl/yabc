75baf7dec3e228e1d4f83af2400771a25bafb2ed
Robert Karl <robertkarljr@gmail.com>
2019-09-12 14:42:13 -0700

    a

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 import setuptools
+"""
+
+# we do not support python3.4 because 
+- dateutil for 3.4 doesn't have parser
+- we need the typing backport for 3.4
+- possible more issues
+"""
 
 setuptools.setup(
     version="0.1.11",
     name="yabc",
-    python_requires=">=3.4,<=3.8",
+    python_requires=">=3.5,<=3.8",
     author="Seattle Blockchain Solutions",
     maintainer="Robert Karl",
     maintainer_email="robertkarljr@gmail.com",
-    install_requires=["flask==1.0.4", "sqlalchemy==1.3.3", "delorean==1.0.0", 'typing'], # typing for 3.4
+    install_requires=["flask==1.0.4", "sqlalchemy==1.3.3", "delorean==1.0.0"],
     test_suite="tests",
     url="https://github.com/robertkarl/yabc",
     description="A tax estimator for cryptocurrencies.",
@@ -24,6 +31,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
 )
