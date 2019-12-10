878ca325bfb460d0a86174dd60f3e5b0ee3511b4
Robert Karl <robertkarljr@gmail.com>
2019-12-10 11:24:24 -0800

    Top level yabc module objects for commonly used things

--- src/yabc/__init__.py
+++ src/yabc/__init__.py
@@ -1,8 +1,13 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 
+from . import server
+
 Base = declarative_base(sqlalchemy.schema.ThreadLocalMetaData())
 
 import yabc.server.yabc_api  # isort:skip
 
 bp = yabc.server.yabc_api.bp
+
+api = server.yabc_api
+get_db = server.sql_backend.get_db
