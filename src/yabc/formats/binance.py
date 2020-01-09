56c2c1f1498862a89edf20ab0a8fe8d9396d519e
Robert Karl <robertkarl@users.noreply.github.com>
2020-01-08 18:47:57 -0800

    Binance xlsx (#100)
    # test plan
    added new tests to read XLSX files
    Note that binance xlsx records seem to include strings

--- src/yabc/formats/binance.py
+++ src/yabc/formats/binance.py
@@ -4,22 +4,77 @@
 Imports binance exchange data into yabc's exchange-independent format.
 
 yabc is not affiliated with the exchange or company Binance.
-
-TODO: binance is the first supported format with coin/coin exchange defined.
-      Once we have historical price data, we can take this live.
 """
 import datetime
 import decimal
-from csv import DictReader
 from typing import Sequence
 
 import delorean
+import openpyxl
 
 from yabc import transaction
 from yabc.formats import FORMAT_CLASSES
 from yabc.formats import Format
 
-HEADERS = "Date,Market,Type,Price,Amount,Total,Fee,Fee Coin".split(",")
+_HEADERS = (
+    "Date(UTC)",
+    "Market",
+    "Type",
+    "Price",
+    "Amount",
+    "Total",
+    "Fee",
+    "Fee Coin",
+)
+
+_KNOWN_COINS = {
+    "ADA",
+    "ADX",
+    "ALGO",
+    "AMB",
+    "AST",
+    "ATOM",
+    "BAT",
+    "BCH",
+    "BCPT",
+    "BNB",
+    "BSV",
+    "BTC",
+    "DASH",
+    "DGD",
+    "DOCK",
+    "DOGE",
+    "ENG",
+    "EOS",
+    "ETC",
+    "ETH",
+    "FTT",
+    "GVT",
+    "ICX",
+    "IOTA",
+    "LINK",
+    "LTC",
+    "LUN",
+    "MATIC",
+    "MOD",
+    "MTL",
+    "NANO",
+    "NEO",
+    "ONT",
+    "PPT",
+    "TOMO",
+    "TRX",
+    "USDT",
+    "VEN",
+    "VET",
+    "WAVES",
+    "WTC",
+    "XLM",
+    "XMR",
+    "XRP",
+    "XTZ",
+}
+
 
 _BINANCE_TYPE_MAP = {
     "SELL": transaction.Operation.SELL,
@@ -29,14 +84,36 @@ _BINANCE_TYPE_MAP = {
 _BINANCE_EXCHANGE_ID_STR = "binance"
 
 
+class BinanceMarket:
+    """ Parse coin/coin market names from Binance cells. Slightly complicated
+    by the fact that some of the symbols have 4 characters
+    """
+
+    def __init__(self, market):
+        possible_lengths = [3, 4, 5]
+        for curr_len in possible_lengths:
+            if market[:curr_len] in _KNOWN_COINS:
+                self._first = market[:curr_len]
+                self._second = market[curr_len:]
+                return
+        raise RuntimeError("Could not parse transaction from market {}".format(market))
+
+    def first(self):
+        return self._first
+
+    def second(self):
+        return self._second
+
+
 def _transaction_from_binance_dict(
     date, market, operation, amount, total, fee, feecoin
 ):
     # type: (datetime.datetime, str, transaction.Operation, decimal.Decimal, decimal.Decimal, decimal.Decimal, str)-> Sequence
 
     # The following fields are accurate if the tx is a BUY and not coin/coin.
-    symbol_traded = market[:3]
-    symbol_received = market[3:]
+    bm = BinanceMarket(market)
+    symbol_traded = bm.first()
+    symbol_received = bm.second()
     quantity_traded = amount
     quantity_received = total
     is_coin_to_coin = not transaction.is_fiat(
@@ -62,28 +139,52 @@ def _transaction_from_binance_dict(
     )
 
 
+def _raise_if_headers_bad(row):
+    for i, h in enumerate(_HEADERS):
+        if not row[i].value == h:
+            raise RuntimeError(
+                "Not a valid Binance valid, header {} found".format(row[i].value)
+            )
+
+
+def _tx_from_binance_row(line):
+    date = delorean.parse(line[0].value).datetime
+    market = line[1].value
+    operation = _BINANCE_TYPE_MAP[line[2].value]
+    amount = decimal.Decimal(line[4].value)
+    total = decimal.Decimal(line[5].value)
+    fee = decimal.Decimal(line[6].value)
+    fee_coin = line[7].value
+    return _transaction_from_binance_dict(
+        date, market, operation, amount, total, fee, fee_coin
+    )
+
+
+def _read_binance_txs_from_file(f):
+    ans = []
+    f.seek(0)
+    workbook = openpyxl.load_workbook(f)
+    sheet = workbook.active
+    all_contents = list(sheet.rows)
+    _raise_if_headers_bad(all_contents[0])
+    contents = all_contents[1:]
+    for row in contents:
+        item = _tx_from_binance_row(row)
+        if item is not None:
+            ans.append(item)
+    return ans
+
+
 class BinanceParser(Format):
     EXCHANGE_HUMAN_READABLE_NAME = "Binance"
     _EXCHANGE_ID_STR = _BINANCE_EXCHANGE_ID_STR
 
+    @staticmethod
+    def needs_binary():
+        return True
+
     def parse(self):
-        reader = DictReader(self._file)
-        for line in reader:
-            for key in HEADERS:
-                if key not in line:
-                    raise RuntimeError("Not a valid binance file.")
-            date = delorean.parse(line["Date"]).datetime
-            market = line["Market"]
-            operation = _BINANCE_TYPE_MAP[line["Type"]]
-            amount = decimal.Decimal(line["Amount"])
-            total = decimal.Decimal(line["Total"])
-            fee = decimal.Decimal(line["Fee"])
-            fee_coin = line["Fee Coin"]
-            self._reports.append(
-                _transaction_from_binance_dict(
-                    date, market, operation, amount, total, fee, fee_coin
-                )
-            )
+        self._reports = _read_binance_txs_from_file(self._file)
 
     def __init__(self, file=None, filename: str = None):
         self._file = file
--- src/yabc/ohlcprovider.py
+++ src/yabc/ohlcprovider.py
@@ -12,14 +12,20 @@ def _make_ohlc(ohlc_str):
     return OhlcData(*[decimal.Decimal(i) for i in ohlc_str.split()])
 
 
+def _make_date(date_str):
+    return datetime.datetime(*[int(i) for i in date_str.split()]).date()
+
+
 jan_1 = datetime.datetime(2017, 1, 1).date()
 _ETH_DATA = {
     jan_1: _make_ohlc("8.5 8.6 8.0 8.1"),
     datetime.datetime(2018, 5, 22).date(): _make_ohlc("700 701 644 648"),
+    _make_date("2019 4 16"): _make_ohlc("162 168 161 168"),
 }
 _BTC_DATA = {
     jan_1: _make_ohlc("1000 1008.6 990 1000"),
-    datetime.datetime(2018, 5, 22).date(): _make_ohlc("8420 8423 8005 8042"),
+    _make_date("2018 5 22"): _make_ohlc("8420 8423 8005 8042"),
+    _make_date("2019 4 16"): _make_ohlc("5066 5238 5055 5235"),
 }
 _PRICE_DATA = {"ETH": _ETH_DATA, "BTC": _BTC_DATA}
 
@@ -43,4 +49,4 @@ class OhlcProvider:
             val = _PRICE_DATA[symbol][dt]
             return val
         except KeyError:
-            raise RuntimeError("No price data found.")
+            raise RuntimeError("No price data found for {} on {}.".format(symbol, dt))
