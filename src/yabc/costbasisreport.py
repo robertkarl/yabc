52c377855d880c49f6ea06525c18df1f46654666
Robert Karl <robertkarljr@gmail.com>
2020-02-18 19:07:10 -0800

    Allow writing CBR to a CSV

--- src/yabc/costbasisreport.py
+++ src/yabc/costbasisreport.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import decimal
 from collections import defaultdict
@@ -13,6 +14,8 @@ from sqlalchemy import orm
 
 import yabc
 from yabc.transaction import PreciseDecimalString
+
+CSV_ROWS = ['Description of Property', 'Date Acquired', 'Date Sold', 'Proceeds', 'Cost Basis', 'Gain (or loss)']
 
 
 class CostBasisReport(yabc.Base):
@@ -33,6 +36,13 @@ class CostBasisReport(yabc.Base):
         ...
     ValueError: Basis and proceeds must both be non-negative. asset is BTC
     """
+    def persist_to_csv_writer(self, writer: csv.DictWriter):
+        writer.writerow({CSV_ROWS[0]: self.asset_name,
+                         CSV_ROWS[1]: str(self.date_purchased),
+                         CSV_ROWS[2]: str(self.date_sold),
+                         CSV_ROWS[3]: str(self.proceeds),
+                         CSV_ROWS[4]: str(self.basis),
+                         CSV_ROWS[5]: str(self.get_gain_or_loss())})
 
     _fields = [
         "id",
